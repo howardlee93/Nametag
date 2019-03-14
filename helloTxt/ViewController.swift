f9329cd78c65f0a96c713bc23142bf1ad29f24d0
Howard Lee <howardl93@yahoo.com>
2019-03-13 20:54:35 -0700

    Initial Commit

---
+++ helloTxt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  helloTxt
+//
+//  Created by howard lee on 3/13/19.
+//  Copyright © 2019 howard lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
